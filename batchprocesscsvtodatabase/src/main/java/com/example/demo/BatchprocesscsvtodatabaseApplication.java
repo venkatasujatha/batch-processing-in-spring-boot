a7247cadaabdb1b761a97842566dd9c731d51dda
sujatha <sujatha.valluru@tectoro.com>
2022-07-04 17:00:13 +0530

    batch processing spring boot

---
+++ batchprocesscsvtodatabase/src/main/java/com/example/demo/BatchprocesscsvtodatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchprocesscsvtodatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchprocesscsvtodatabaseApplication.class, args);
+	}
+
+}
